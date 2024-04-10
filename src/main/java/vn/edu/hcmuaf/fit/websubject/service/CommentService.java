5f7339d33b2cddc7f7c03bba2d99aa03d332d122
hoang20130267 <20130267@st.hcmuaf.edu.vn>
2024-04-10 20:15:26 +0700

    them phuong thuc lay danh sach comment

--- src/main/java/vn/edu/hcmuaf/fit/websubject/service/CommentService.java
+++ src/main/java/vn/edu/hcmuaf/fit/websubject/service/CommentService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentService {
+    List<Comment> getListCommentByProductId(int idProduct);
     List<Comment> getListCommentByUserId(int idUser);
     void addComment(int idProduct, int rate, String description);
     void deleteComment(int idComment);
